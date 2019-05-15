4659b67aa9ab62481bcca58a2bf1c76a166d02e4
daisuke13 <people.in.the.box.13@gmail.com>
2019-05-15 20:19:39 +0900

    import echo

--- server/src/app/main.go
+++ server/src/app/main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"log"
 	"net/http"
+
+	"github.com/labstack/echo"
 )
 
 func main() {
-	http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Hello World!")
-	}))
-	log.Println("Now server is running on port 3000")
-	http.ListenAndServe(":3000", nil)
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
 }
