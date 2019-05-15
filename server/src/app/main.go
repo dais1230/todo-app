118d60e73d8361aeff2026c232bebcf666227abd
daisuke13 <people.in.the.box.13@gmail.com>
2019-05-15 22:03:15 +0900

    installed gorm and initial migration

--- server/src/app/main.go
+++ server/src/app/main.go
@@ -1,15 +1,55 @@
 package main
 
 import (
-	"net/http"
+	"fmt"
+	"log"
 
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
+// Association
+type User struct {
+	gorm.Model
+	Name  string `gorm:"size:255"`
+	Email string
+	Task  []Task
+}
+
+type Task struct {
+	gorm.Model
+	UserID      int    `gorm:"index"`
+	Name        string `gorm:"size:255"`
+	Description string `gorm:"type:text"`
+}
+
 func main() {
+	initMigrate()
+	run()
+}
+
+func initMigrate() {
+	db, err := gorm.Open("mysql", "root@/todoapp?charset=utf8&parseTime=True&loc=Local")
+	defer db.Close()
+	if err != nil {
+		panic(err)
+	}
+	db.Set("gorm:table_options", "ENGINE=InnoDB").AutoMigrate(&User{}, &Task{})
+}
+
+func run() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
-	e.Logger.Fatal(e.Start(":1323"))
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// e.GET("/users", showUsers)
+	// e.GET("/user/:id", showUser)
+	// e.POST("/user", newUser)
+	// e.PUT("/user/:id", updateUser)
+	// e.DELETE("/user/:id", deleteUser)
+
+	fmt.Println("Run server with echo localhost:1313")
+	log.Fatal(e.Start(":1313"))
 }
